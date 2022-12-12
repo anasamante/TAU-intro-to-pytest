c2af3a645913947e63579ac0c7cabb105fbee2f4
Ana <ana.s.amante@gmail.com>
2022-12-12 12:00:39 -0300

    added Exception test case

--- test/test_math.py
+++ test/test_math.py
@@ -1,9 +1,27 @@
+# passing test
+import pytest as pytest
+
+
 def test_one_plus_one():
     assert 1 + 1 == 2
 
 
+# failing test
 def test_one_plus_two():
     a = 1
     b = 2
-    c = 3
+    c = 0
     assert a + b == c
+
+
+# exception test
+def test_divide_by_zero():
+    num = 1 / 0
+
+
+# exception test with .raises, verifies an exception
+def test_divide_by_zero_exception():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1 / 0
+
+    assert 'division by zero' in str(e.value)
