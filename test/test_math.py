9f9d76bda123685abdf118bd816d65a3c344256c
Ana <ana.s.amante@gmail.com>
2022-12-12 17:04:08 -0300

    added parameterized cases

--- test/test_math.py
+++ test/test_math.py
@@ -25,3 +25,22 @@ def test_divide_by_zero_exception():
         num = 1 / 0
 
     assert 'division by zero' in str(e.value)
+
+
+# parameterized cases
+# multiplication cases
+
+# create a list of tuple with the same length
+product = [
+    (2, 3, 6),  # two positive int
+    (1, 99, 99),  # any number by one
+    (0, 99, 0),  # by zero
+    (3, -4, -12),  # positive by a negative
+    (-5, -5, 25),  # negative by a negative
+    (2.5, 6.7, 16.75)  # multiply floats
+]
+
+
+@pytest.mark.parametrize('a, b, product', product)
+def test_multiply(a, b, product):
+    assert a * b == product
